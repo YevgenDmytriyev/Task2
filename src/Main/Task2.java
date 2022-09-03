26e34e9e47a509411ef13207abd389020881f389
User <dmitrievevgenii@gmail.com>
2022-09-03 16:14:20 +0300

    Initial commit

---
+++ src/Main/Task2.java
@@ -0,0 +1,12 @@
+package Main;
+
+public class Task2 {
+    public static void main(String[] args) {
+
+        double myDouble = 52.55375696959752;
+        System.out.println(myDouble);
+
+        double myDouble2 = 13.467574896368196;
+        System.out.println(myDouble2);
+    }
+}
